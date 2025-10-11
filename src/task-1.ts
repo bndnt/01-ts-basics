a3c5af076bc7b4b883c1812cf62152e261d55439
bndnt <67003594+bndnt@users.noreply.github.com>
2025-10-10 23:54:59 -0400

    ts homework 1-6

---
+++ src/task-1.ts
@@ -0,0 +1,7 @@
+const name: string = "Alice";
+const age: number = 30;
+const isOnline: boolean = true;
+
+console.log(`Name: ${name} type: ${typeof name}`);
+console.log(`Age: ${age} type: ${typeof age}`);
+console.log(`Online: ${isOnline} type:${isOnline}`);
